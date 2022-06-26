d0fa1e0880fe2e2238f30fc15c6c6a86511f33b4
dowries <108212877+dowries@users.noreply.github.com>
2022-06-26 02:31:59 -0700

    Update noscroll.js

--- noscroll.js
+++ noscroll.js
@@ -1,4 +1,10 @@
-$('input').on('focus', function(e) {
-    e.preventDefault(); e.stopPropagation();
-    window.scrollTo(0,0); //the second 0 marks the Y scroll pos. Setting this to i.e. 100 will push the screen up by 100px. 
-});
+ $(document).ready(function() {
+     document.ontouchmove = function(e){
+          e.preventDefault();
+          }
+ });
+
+input.onfocus = function () {
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+}
