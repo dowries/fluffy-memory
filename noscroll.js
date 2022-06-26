55d95b62d6f2fbf5f62d9124eb39e05509070887
dowries <108212877+dowries@users.noreply.github.com>
2022-06-26 01:48:40 -0700

    Update noscroll.js

--- noscroll.js
+++ noscroll.js
@@ -1,12 +1,1 @@
-$(function() {
-    var oldScroll = window.onscroll;
-    $(document).on('focus', 'input', function(e) {
-        window.onscroll = function () { 
-            window.scroll(0,0); 
-        } ;
-        setTimeout(function() {
-            window.onscroll = oldScroll;
-        }, 100);
-    });
-
-});
+document.getElementById('text, textbox, textarea, hidden').onkeydown = new function(event){return false}
